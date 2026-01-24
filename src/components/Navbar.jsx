c861263be94fc379d8ee435ea8d9b9a781f0ac2d
SHRESTHA-012 <deyshrestha42@gmail.com>
2026-01-24 13:23:58 +0530

    Some new components and pages have been added

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -13,7 +13,7 @@ const Navbar = () => {
             </NavLink>
              <NavLink to="/projects" className={({ isActive}) => isActive ?
             'text-blue-500' : 'text-black'}>
-              Project
+              Projects
             </NavLink>
         </nav>
     </header> 
