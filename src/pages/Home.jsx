5e7563277c10ab80fe2aafebfa5b3c51171fdca0
SHRESTHA-012 <deyshrestha42@gmail.com>
2026-01-25 17:45:50 +0530

    minor additions like assets and models.

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -16,6 +16,8 @@ const Home = () => {
         camera={{near : 0.1 , far : 1000}}
         >
             <Suspense fallback={<Loader />}>
+
+            
             </Suspense>
 
         </Canvas>
