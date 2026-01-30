97c13ac91e71bb0ca8600ebed7e20d1f5685b684
SHRESTHA-012 <deyshrestha42@gmail.com>
2026-01-30 22:33:36 +0530

    Started adding and modifying the About component.

--- src/pages/About.jsx
+++ src/pages/About.jsx
@@ -2,7 +2,17 @@ import React from 'react'
 
 const About = () => {
   return (
-    <div>About</div>
+    <section className="max-container">
+      <h1 className="head-text">
+        Hello, I'm <span className="blue-gradient_text font-semibold
+        drop-shadow">Shrestha</span>
+      </h1>
+
+      <div>
+        <p>Software Engineering student based in India, specializing in
+        AIML.</p>
+      </div>
+    </section>
   )
 }
 
